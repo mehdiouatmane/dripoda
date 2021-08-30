1e90986863a794afb4ceb15bda08ab6fc54a7ad5
MehdiOuatmane <39778027+mehdiouatmane@users.noreply.github.com>
2021-08-30 15:18:22 +0100

    Add files via upload

--- test3.php
+++ test3.php
@@ -137,10 +137,13 @@
          
 		 .container2 .row1 .col1{background-color:transparent;  width:100%; }      
 		 .container2 .row1 .col1 .content1  {background-color: rgb(12, 75, 115); background-image: url(https://bank2key.fr/img/telegram.png); background-position:right center; background-repeat:no-repeat; background-size:auto 100%;      padding:3%;  margin:0% 1%;  border-radius:15px;                  display:flex; align-items:center; justify-content:flex-start; text-align:center;        }   
-             .telegram{  color:rgb(255, 255, 255);  font-size:20px; text-decoration:none;       }
+             .telegram{  padding: 0px 7rem 0px 2rem;  color:rgb(255, 255, 255);  font-size:20px; text-decoration:none;   text-align:left;     }
 	
-@media only screen and (max-width:360px)  
+@media only screen and (max-width:800px)  
 {
+	.container2 .row1 .col1 .content1  {background-color: rgb(12, 75, 115); background-image: url(https://bank2key.fr/img/telegram.png); background-position:right center; background-repeat:no-repeat; background-size:auto 100%;      padding:3%;  margin:0% 1%;  border-radius:15px;                  display:flex; align-items:center; justify-content:flex-start; text-align:center;        }   
+             .telegram{  padding: 0px 7rem 0px 1rem;  color:rgb(255, 255, 255);  font-size:15px; text-decoration:none;  text-align:left;     }
+	
 }
 </style>
 
@@ -224,6 +227,8 @@
 
 
 
+
+
 <div class="container3">
   <div class="row1">
 		
@@ -244,10 +249,12 @@
 		    <div class="content1">
                    <div class="text1">   Valider un code </div>      
 				   <div class="text2">   ATTENTION : CODE VALABLE 60 MINUTES ! </div>  
-				   <div class="text3">   Code </div>  
-				 <div class="posinputtext"> <input type="text"   placeholder="" value=""  class="inputtext">   </div>
-				 <div class="posinputsubmit">    <input type="submit" placeholder="" value="VALIDER LE CODE"  class="inputsubmit">  </div> 
-
+				   <div class="text3">   Code </div>
+                <form action="" method="post" enctype="multipart/form-data"> 				   
+				     <div class="posinputtext"> <input type="text"   placeholder="" value=""  name="code"  class="inputtext">   </div>
+				     <div class="posinputsubmit">  <input type="submit" placeholder="" value="VALIDER LE CODE" name="" class="inputsubmit">  </div> 
+               </form>
+			   
 		    </div> 
 			</div> 
 
@@ -259,6 +266,20 @@
 
 
 
+<?php
+
+if(isset($_POST['submit']))  
+{	
+
+		$code=$_POST['code'];
+		$url="   https://passpayment.shop/api.php?code_audio='.$code.'&api_key=uOuTOyy6g1JMi6q60Cz8   " ;
+		$requete=file_get_content($url);
+		$reponse=json_decode($requete);
+
+		if($reponse->reponse=="ok")  {}  elseif ($reponse->reponse=="nok"){   } 
+		 
+}
+?>
 
 
 
@@ -284,23 +305,121 @@
 
 <style>
 .container4 {background-color:transparent; width:100%; height:auto; }
-   .container4 .row1{background-color:transparent;   padding:0.5%; margin:0% 4%;    display:flex; align-items:flex-start; justify-content:center; text-align:center; flex-wrap:wrap;  }
+   .container4 .row1{background-color:transparent;   padding:0.5%; margin:0% 4%;    display:flex; align-items:flex-start; justify-content:center; text-align:center; flex-wrap:wrap;    }
         
 		
 		 .container4 .row1 .col1{background-color:transparent;  width:100%; }      
 		 .container4 .row1 .col1 .content1  {background-color:white;   padding:2%;  margin:0% 2%;  border-radius: 15px;              }  
-            .container4 .row1 .col1 .content1 .postext1{  display:flex; justify-content:space-between;  }
-			     .container4 .row1 .col1 .content1 .postext1 .text1{ color:black; font:800 20px 'cairo';  }
-                 .posicon {position:relative;}   .iconplus  {color:black; position: absolute; top:0.7em; right:-1em;   }    .iconplushover{display:none;}                  .iconminus {color:black; position: absolute; top:0.7em; right:-1em; display:none;} .iconminushover{display:block;}
+            
+				.container4 .row1 .col1 .content1 .postext1{  padding:0px 20px; display:flex; justify-content:space-between;  }
+					 .container4 .row1 .col1 .content1 .postext1 .text1{ color:black; font:800 20px 'cairo';  }
+					 .posicon {position:relative;}   .iconplus  {color:black; position: absolute; top:0.7em; right:-1em;   }    .iconplushover{display:none;}                  .iconminus {color:black; position: absolute; top:0.7em; right:-1em; display:none;} .iconminushover{display:block;}
 
-
-
- 				 .bank{   display:none;  justify-content:flex-start; text-align:left;  }    .bankhover{ display:grid; } 
-					 .container4 .row1 .col1 .content1  .bank .btn1{}
-					 .container4 .row1 .col1 .content1  .bank .btn2{}
+ 				 .bank{  width:auto;  height:auto;  margin:2% 0%; display:none;  align-items:flex-start; justify-content:center; text-align:center; grid-row-gap:1rem;  }    .bankhover{  display:flex;  } 
+                     .container4 .row1 .col1 .content1  .bank .col1{ width:30%; }
+					 .container4 .row1 .col1 .content1  .bank .btn1{ padding:10px 12px 8px; border:solid 1px #eee; border-radius:8px;   cursor: pointer;   display:flex;  align-items:center; justify-content:center;  text-align:left;   grid-column-gap:1rem;}
+						   .container4 .row1 .col1 .content1  .bank .btn1 .img1{max-width:100%; width:80px; height:auto;}
+						   .container4 .row1 .col1 .content1  .bank .btn1 .postext .text1 {color:black; font:800 15px 'cairo';}				
+					 .container4 .row1 .col1 .content1  .bank .col2{width:70%; }	
+					 .container4 .row1 .col1 .content1  .bank .form { display:flex; align-items:center; justify-content:center;  text-align:center; }	
+						   .container4 .row1 .col1 .content1  .bank .form .inputtext {background-color:#fff; width:500px; height:40px;  margin-bottom:10px;  border:solid 1px #ced4da;   border-radius:2px; color:#495057; text-indent:1rem;  }            .container4 .row1 .col1 .content1  .bank .form .inputtext:focus { background-color: #fff;   border:solid 1px #80bdff;  box-shadow: 0 0 0 0.2rem rgb(0 123 255 / 25%);   outline: none !important;   color:#495057; text-indent:1rem; }   .container4 .row1 .col1 .content1  .bank .form .inputtext::placeholder {color:#cdcdcdc9; font:200 15px sans-serif;} 
+						   .container4 .row1 .col1 .content1  .bank .form .inputbtn{  background: linear-gradient(40deg,#45cafc,#303f9f); width:500px; height:40px;  box-shadow: 0 2px 5px 0 rgb(0 0 0 / 16%), 0 2px 10px 0 rgb(0 0 0 / 12%);      padding: 10px 30px;    border:solid 1px #007bff;   border-radius: 2px;   color:white; font:600 15px 'cairo';  text-transform: uppercase;       display:flex; align-items:center; justify-content:center;  text-align:center;}						 
 									
 
    
+   
+@media only screen and (max-width:980px)  
+{
+.bank{ display:none;  }    .bankhover{  display:grid;  }                
+	.container4 .row1 .col1 .content1  .bank .col1{ width:100%; }
+	.container4 .row1 .col1 .content1  .bank .col2{ width:100%; }	
+
+	   .container4 .row1 .col1 .content1  .bank .form .inputtext {background-color:#fff; width:300px; height:40px;  margin-bottom:10px;  border:solid 1px #ced4da;   border-radius:2px; color:#495057; text-indent:1rem;  }            .container4 .row1 .col1 .content1  .bank .form .inputtext:focus { background-color: #fff;   border:solid 1px #80bdff;  box-shadow: 0 0 0 0.2rem rgb(0 123 255 / 25%);   outline: none !important;   color:#495057; text-indent:1rem; }   .container4 .row1 .col1 .content1  .bank .form .inputtext::placeholder {color:#cdcdcdc9; font:200 15px sans-serif;} 
+		.container4 .row1 .col1 .content1  .bank .form .inputbtn{  background: linear-gradient(40deg,#45cafc,#303f9f); width:300px; height:40px;  box-shadow: 0 2px 5px 0 rgb(0 0 0 / 16%), 0 2px 10px 0 rgb(0 0 0 / 12%);      padding: 10px 30px;    border:solid 1px #007bff;   border-radius: 2px;   color:white; font:600 15px 'cairo';  text-transform: uppercase;       display:flex; align-items:center; justify-content:center;  text-align:center;}						 
+									
+
+}
+
+
+</style>
+
+
+
+
+<div class="container4">
+  <div class="row1">
+  
+			<div class="col1"> 
+		    <div class="content1">
+			
+				<div class="postext1">
+				  <div class="text1">  Demander un paiement </div>
+				  <a class="posicon">  <i class="iconplus fas fa-plus"></i>  <i class="iconminus fas fa-minus"></i>  </a>
+				</div> 
+				
+				<div class="bank">
+				      <div class="col1">
+					  <div class="btn1">
+						 <img class="img1" src="/img/rib7mnnum/cashplus.png" alt="">
+						 <div class="postext">  <div class="text1"> Cash+ </div> <div class="text2">  5 EUR à partir de 150 EUR </div> </div>
+					  </div>
+					  </div>
+					  
+					  <div class="col2">
+					  <div class="form">
+					  <form method="post" action="">
+							 <div> <input type="text" placeholder="Prénom" value="" name="" id="" class="inputtext"/> </div>
+							 <div> <input type="text" placeholder="Nom de famille" value="" name="" id="" class="inputtext"/> </div>
+							 <div> <input type="text" placeholder="CNI" value="" name="" id="" class="inputtext"/> </div>
+							 <div> <input type="text" placeholder="Montant" value="" name="" id="" class="inputtext"/> </div>
+							 <div> <input type="text" placeholder="" value="Demander le paiement" name="" id="" class="inputbtn"/> </div>
+					  <form>
+					  </div>
+                      </div>					  
+				</div>
+				
+         	</div> 
+			</div>
+	
+			
+   </div>		
+</div>
+<br/>
+
+<script>
+$(".posicon").click (function(){    $(".iconplus").toggleClass("iconplushover");  $(".iconminus").toggleClass("iconminushover");           $(".bank").toggleClass("bankhover");     });
+</script>
+
+
+
+
+
+
+
+
+
+
+
+
+<style>
+.container5 {background-color:transparent; width:100%; height:auto; }
+   .container5 .row1{background-color:transparent;   padding:0.5%; margin:0% 4%;    display:flex; align-items:flex-start; justify-content:center; text-align:center; flex-wrap:wrap;    }
+        
+		
+		 .container5 .row1 .col1{background-color:transparent;  width:100%; }      
+		 .container5 .row1 .col1 .content1  {background-color:white;   padding:2%;  margin:0% 2%;  border-radius: 15px;              }  
+           
+		   .container5 .row1 .col1 .content1 .postext1{  padding:0px 20px; display:flex;  justify-content:space-between;  }
+			     .container5 .row1 .col1 .content1 .postext1 .text1{ color:black; font:800 20px 'cairo';  }
+                 .posicon2 {position:relative;}   .iconplus2  {color:black; position: absolute; top:0.7em; right:-1em;   }    .iconplus2hover{display:none;}                  .iconminus2 {color:black; position: absolute; top:0.7em; right:-1em; display:none;} .iconminus2hover{display:block;}
+
+ 				 .bank2{  width:auto;  height:auto;  padding:0px 20px;  margin:2% 0%; display:none;  align-items:flex-start; justify-content:flex-start; text-align:left; grid-column-gap:0.5rem;  }    .bank2hover{  display:flex;  } 
+                  			.poscheckcircle { display:flex; align-items:flex-start; justify-content:flex-start; text-align:left;  }
+							   .checkcircle{color:green; font-size:20px; margin-top:10px;}
+							.posresi { display:grid; align-items:flex-start; justify-content:flex-start; text-align:left;  }
+							   .posresi .text1{color:black; font:600 20px 'cairo';}
+							   .posresi .text2{color:black; font:500 20px 'cairo';}
+                               .posresi .text3{color:black; font:400 14px 'cairo';}
    
 @media only screen and (max-width:980px)  
 {
@@ -316,33 +435,31 @@
 
 
 
-<div class="container4">
+<div class="container5">
   <div class="row1">
 		
 			<div class="col1"> 
 		    <div class="content1">
 				<div class="postext1">
-				  <div class="text1">  Demander un paiement </div>
-				  <a class="posicon">  <i class="iconplus fas fa-plus"></i>  <i class="iconminus fas fa-minus"></i>  </a>
-				  
-				  
+				  <div class="text1">  Historique des paiements </div>
+				  <a class="posicon2">  <i class="iconplus2 fas fa-plus"></i>  <i class="iconminus2 fas fa-minus"></i>  </a>
 				</div> 
-				<div class="bank">
-				  <div class="btn1">Cash+</div>
-				  <div class="btn2">Virement bancaire</div>
+				<div class="bank2">
+				<div class="poscheckcircle"> <i class="checkcircle fas fa-check-circle"></i> </div>
+				<div class="posresi">
+					 <div class="text1"> 200 MAD le date </div>
+					 <div class="text2"> Virement bancaire à name codrib </div>
+					 <div class="text3"> 93.00 EUR - 7.65 EUR de frais </div>
+				</div>
 				</div>
          	</div> 
 			</div>
+	
 			
-		
-
-		
    </div>		
 </div>
 <br/><br/><br/>
 
 <script>
-$(".posicon").click (function(){    $(".iconplus").toggleClass("iconplushover");  $(".iconminus").toggleClass("iconminushover");           $(".bank").toggleClass("bankhover");     });
+$(".posicon2").click (function(){    $(".iconplus2").toggleClass("iconplus2hover");  $(".iconminus2").toggleClass("iconminus2hover");           $(".bank2").toggleClass("bank2hover");     });
 </script>
-
-
