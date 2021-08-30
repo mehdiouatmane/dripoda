6e29cad2442de9e8f26dcf3cd81f14d34ade749f
MehdiOuatmane <39778027+mehdiouatmane@users.noreply.github.com>
2021-08-30 10:31:09 +0100

    Add files via upload

--- test2.php
+++ test2.php
@@ -26,7 +26,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"  rel="stylesheet"  > <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" ></script>  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" ></script>  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" ></script>
 
 
-<style>html, body{ background-color:transparent;  padding:0; border:0; margin:0;   touch-action: none;    user-select: none;   }   ul li {list-style-type:none;}     </style>
+<style>html, body{ background-color:transparent;  padding:0; border:0; margin:0;  touch-action: manipulation;    user-select: none;   }   ul li {list-style-type:none;}     </style>
 
 
 <!-- Facebook Pixel Code -->	
