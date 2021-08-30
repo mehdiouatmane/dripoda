3898ab6d4e0a54efcb26e129f86c6e1859b08f97
MehdiOuatmane <39778027+mehdiouatmane@users.noreply.github.com>
2021-08-30 10:31:34 +0100

    Add files via upload

---
+++ test3.php
@@ -0,0 +1,348 @@
+<title>Bank2Key</title>
+<meta charset="utf-8">   <meta http-equiv="X-UA-Compatible" content="IE=edge"/>  <meta http-equiv="origin-trial" content=""/>  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no"/>     
+
+
+<script type="text/javascript"  src="https://code.jquery.com/jquery-3.5.1.min.js"></script> 
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.css"/>  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Cairo&display=swap" />
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css"/>   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.theme.default.min.css"/>   <script type="text/javascript"  src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js"      ></script>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"  rel="stylesheet"  > <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.bundle.min.js" ></script>  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.3/dist/umd/popper.min.js" ></script>  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/js/bootstrap.min.js" ></script>
+
+
+<style>html, body{  background-color:#f5f4f5;  padding:0; border:0; margin:0;  touch-action: manipulation; }   ul li {list-style-type:none;}     </style>
+
+
+
+
+
+
+
+
+
+
+
+
+
+<style>
+.container0 {background-color:black; width:100%; height:auto; }
+   .container0 .row1{background-color:transparent;   padding:0.5%; margin:0% 5%;    display:flex; align-items:center; justify-content:center; text-align:center; flex-wrap:wrap;  }
+         
+		 .container0 .row1 .col1{background-color:transparent;  width:50%; }      
+		 .container0 .row1 .col1 .content1  {background-color:transparent; padding:0%; margin:0% 0%;         display:flex; align-items:center; justify-content:flex-start; text-align:center;        }  
+               .logo{color:white; font:800 20px 'cairo';}
+		 .container0 .row1 .col2{background-color:transparent;  width:50%; }      
+		 .container0 .row1 .col2 .content1   {background-color:transparent;  padding:0%; margin:0% 0%;         display:flex; align-items:center; justify-content:flex-end; text-align:center;   grid-column-gap:2rem;     }  
+              .iconeuser{display:flex; align-items:center; justify-content:center; text-align:center;}  .iconeuser i{color:white; font-size:20px;}
+              .iconepower{display:flex; align-items:center; justify-content:center; text-align:center;}  .iconepower i{color:white; font-size:20px;}
+
+ 
+@media only screen and (max-width:360px)  
+{
+}
+</style>
+
+
+
+<div class="container0">
+  <div class="row1">
+		
+			<div class="col1"> 
+		    <div class="content1">
+                   <div class="logo">   Bank2Key  </div> 
+		    </div> 
+			</div>
+			
+			<div class="col2"> 
+		    <div class="content1">
+                    <div class="iconeuser"><i class="fas fa-user"></i> </div>
+					 <div class="iconepower"><i class="fas fa-power-off"></i> </div>
+					
+		    </div> 
+			</div> 
+
+		
+   </div>		
+</div>
+<br/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<style>
+.container1 {background-color:transparent; width:100%; height:auto; }
+   .container1 .row1{background-color:transparent;   padding:0.5%; margin:0% 5%;    display:flex; align-items:center; justify-content:center; text-align:center; flex-wrap:wrap;  }
+         
+		 .container1 .row1 .col1{background-color:transparent;  width:50%; }      
+		 .container1 .row1 .col1 .content1  {background:linear-gradient(40deg ,#ffd86f,#fc6262);   padding:3%;  margin:0% 2%;  border-radius: 15px;       display:grid; align-items:center; justify-content:center; text-align:center;        }  
+                .container1 .row1 .col1 .content1 .prix{color:white; font:800 20px 'cairo';}
+		 .container1 .row1 .col2{background-color:transparent;  width:50%; }      
+		 .container1 .row1 .col2 .content1   {background: linear-gradient(40deg,#ff6ec4,#7873f5);  padding:3%;  margin:0% 2%;  border-radius: 15px;       display:grid; align-items:center; justify-content:center; text-align:center;      }  
+            .container1 .row1 .col2 .content1 .Validé{color:white; font:800 20px 'cairo';}
+ 
+@media only screen and (max-width:360px)  
+{
+}
+</style>
+
+
+
+<div class="container1">
+  <div class="row1">
+		
+			<div class="col1"> 
+		    <div class="content1">
+                   <div class="solde">   SOLDE </div>   <div class="prix"> 0.60 EUR </div> 
+ 
+		    </div> 
+			</div>
+			
+			<div class="col2"> 
+		    <div class="content1">
+                   <div class="dernierpaiement">   DERNIER PAIEMENT </div>      <div class="Validé"> Validé </div>  
+
+		    </div> 
+			</div> 
+
+		
+   </div>		
+</div>
+<br/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<style>
+.container2 {background-color:transparent; width:100%; height:auto; }
+   .container2 .row1{background-color:transparent;   padding:0.5%; margin:0% 5%;    display:flex; align-items:center; justify-content:center; text-align:center; flex-wrap:wrap;  }
+         
+		 .container2 .row1 .col1{background-color:transparent;  width:100%; }      
+		 .container2 .row1 .col1 .content1  {background-color: rgb(12, 75, 115); background-image: url(https://bank2key.fr/img/telegram.png); background-position:right center; background-repeat:no-repeat; background-size:auto 100%;      padding:3%;  margin:0% 1%;  border-radius:15px;                  display:flex; align-items:center; justify-content:flex-start; text-align:center;        }   
+             .telegram{  color:rgb(255, 255, 255);  font-size:20px; text-decoration:none;       }
+	
+@media only screen and (max-width:360px)  
+{
+}
+</style>
+
+
+
+<div class="container2">
+  <div class="row1">
+		
+			<div class="col1"> 
+		    <div class="content1">
+                   <a class="telegram">  Rejoins nous sur le canal officiel Telegram de Bank2Key  </a>    
+		    </div> 
+			</div>
+
+
+		
+   </div>		
+</div>
+<br/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<style>
+.container3 {background-color:transparent; width:100%; height:auto; }
+   .container3 .row1{background-color:transparent;   padding:0.5%; margin:0% 5%;    display:flex; align-items:flex-start; justify-content:center; text-align:center; flex-wrap:wrap;  }
+         
+		 .container3 .row1 .col1{background-color:transparent;  width:50%; }      
+		 .container3 .row1 .col1 .content1  {background-color:white;   padding:2%;  margin:0% 2%;  border-radius: 15px;        display:grid; align-items:center; text-align:left;   flex-direction: column;        }  
+             .container3 .row1 .col1 .content1  .text1 {color:black; font:800 20px 'cairo';   }
+			 .container3 .row1 .col1 .content1  .text2 {background-color:#f8d7da; padding:.75rem 1.25rem;       color:#721c24; font:400 19px 'cairo'; text-indent:2rem;  }
+			 .container3 .row1 .col1 .content1  .text3 {padding:2%; border-bottom:solid 1px  rgba(0,0,0,.125);   display:flex; align-items:center; grid-column-gap:0.5rem; }   
+			     .container3 .row1 .col1 .content1  .text3 .text3a{color:black; font:800 19px 'cairo';}  
+				 .container3 .row1 .col1 .content1  .text3 .text3b{color:rgba(0,0,0,.5); font:400 15px 'cairo';}
+			 .container3 .row1 .col1 .content1  .text4 {padding:2%; border-bottom:solid 1px  rgba(0,0,0,.125); display:flex; justify-content: space-between;  }  
+				   .container3 .row1 .col1 .content1  .text4 .text4ab{ display:flex; align-items:center;   flex-wrap: wrap;  grid-column-gap:1rem; }	
+				     .container3 .row1 .col1 .content1  .text4 .text4a{ color:black; font:800 20px 'cairo';  }
+					 .container3 .row1 .col1 .content1  .text4 .text4b{ color:rgba(0,0,0,.5); font:300 15px 'cairo';  }
+				   .container3 .row1 .col1 .content1  .text4 .text4cd{ display:flex; align-items:center;   flex-wrap: wrap;   flex-direction: column;  grid-column-gap:0.5rem;   }	
+					.container3 .row1 .col1 .content1  .text4 .text4c{color:#444; font:500 20px 'cairo';}
+					.container3 .row1 .col1 .content1  .text4 .text4d{color:rgba(0,0,0,.5); font:300 15px 'cairo'; }			   
+		.container3 .row1 .col1 .content1  .text5 { background-color: #fdfdfd;   box-shadow: inset 0 4px 7px 0 rgb(0 0 0 / 5%);    border-top: 1px solid #e5e5e5;   border-radius: 0 0 15px 15px;       color: rgba(0,0,0,.5);  font:600 15px 'cairo';     display:flex; align-items:center; justify-content:center; text-align:center;}
+		
+		 .container3 .row1 .col2{background-color:transparent;  width:50%; }      
+		 .container3 .row1 .col2 .content1   {background-color:white;    padding:3%;  margin:0% 2%;  border-radius: 15px;       display:grid; align-items:center;  text-align:left; flex-direction: column;     }  
+             .container3 .row1 .col2 .content1  .text1 {color:black; font:800 20px 'cairo';}
+			 .container3 .row1 .col2 .content1  .text2 { background-color:#d4edda;  padding:.75rem 1.25rem;     color:#155724; font:400 19px 'cairo'; }
+             .container3 .row1 .col2 .content1  .text3 {color:#444; font:500 20px 'cairo';}
+	    	 .container3 .row1 .col2 .content1  .posinputtext{ display:flex; align-items:center; justify-content:center; text-align:center; }   .container3 .row1 .col2 .content1  .inputtext {background-color:#fff; width:100%; padding:10px 0; border:solid 1px #ced4da;   border-radius:2px; color:#495057; text-ident:2rem;  margin-bottom:10px;}         .container3 .row1 .col2 .content1  .inputtext:focus { background-color: #fff;   border:solid 1px #80bdff;  box-shadow: 0 0 0 0.2rem rgb(0 123 255 / 25%);   outline: none !important;   color:#495057; text-ident:2rem; }
+             .container3 .row1 .col2 .content1  .posinputsubmit {  display:flex; align-items:center; justify-content:center; text-align:center;}    .container3 .row1 .col2 .content1  .inputsubmit{ background:linear-gradient( 40deg ,#45cafc,#303f9f);   padding:10px 50px;  border:solid 1px #007bff;   border-radius:5px;  box-shadow: 0 2px 5px 0 rgb(0 0 0 / 16%), 0 2px 10px 0 rgb(0 0 0 / 12%);    color:white;  font:700 15px 'cairo';           display:flex; align-items:center; justify-content:center; text-align:center;}
+
+
+
+@media only screen and (max-width:980px)  
+{
+	  .container3 .row1{ display:grid; align-items:flex-start; justify-content:center; text-align:center; flex-wrap:wrap;  grid-row-gap:2rem; } 
+         .container3 .row1 .col1{background-color:transparent;  width:100%; }  
+		 .container3 .row1 .col2{background-color:transparent;  width:100%; }
+	
+}
+
+@media only screen and (max-width:360px)  
+{
+
+          .container3 .row1{padding:0%; margin:0% 0%;   }
+         
+ 
+				
+}
+</style>
+
+
+
+<div class="container3">
+  <div class="row1">
+		
+			<div class="col1"> 
+		    <div class="content1">
+                   <div class="text1">   Obtenir un code </div>    <br/> 
+				   <div class="text2"> ORANGE n'est pas actif </div>  <br/> 
+				   <div class="text3"> <div class="text3a"> 5 appels </div> <div class="text3b">3 codes par client </div> </div> 
+				   <div class="text4">    <div class="text4ab">  <div class="text4a"> 08 98 40 14 65 </div>  <div class="text4b"> +4 EUR </div>  </div>    <div class="text4cd">  <div class="text4c">8136 </div>    <div class="text4d">appels restants</div>    </div> 			   </div>
+				  <div class="text3"> <div class="text3a">2 appels </div> <div class="text3b">1 code par client  </div> </div> 
+				  <div class="text4"> <div class="text4ab">  <div class="text4a">08 98 40 12 61 </div>  <div class="text4b">  +1.6 EUR  </div>  </div>    <div class="text4cd">  <div class="text4c">9857 </div>    <div class="text4d">appels restants</div>    </div>     </div> 
+		          <div class="text5"> 3 CODES PAR CLIENT SUR LE 5 APPELS + 1 CODE PAR CLIENT SUR LE 2 APPELS </div> 
+			</div> 
+			</div>
+			
+			
+			<div class="col2"> 
+		    <div class="content1">
+                   <div class="text1">   Valider un code </div>      
+				   <div class="text2">   ATTENTION : CODE VALABLE 60 MINUTES ! </div>  
+				   <div class="text3">   Code </div>  
+				 <div class="posinputtext"> <input type="text"   placeholder="" value=""  class="inputtext">   </div>
+				 <div class="posinputsubmit">    <input type="submit" placeholder="" value="VALIDER LE CODE"  class="inputsubmit">  </div> 
+
+		    </div> 
+			</div> 
+
+		
+   </div>		
+</div>
+<br/>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<style>
+.container4 {background-color:transparent; width:100%; height:auto; }
+   .container4 .row1{background-color:transparent;   padding:0.5%; margin:0% 4%;    display:flex; align-items:flex-start; justify-content:center; text-align:center; flex-wrap:wrap;  }
+        
+		
+		 .container4 .row1 .col1{background-color:transparent;  width:100%; }      
+		 .container4 .row1 .col1 .content1  {background-color:white;   padding:2%;  margin:0% 2%;  border-radius: 15px;              }  
+            .container4 .row1 .col1 .content1 .postext1{  display:flex; justify-content:space-between;  }
+			     .container4 .row1 .col1 .content1 .postext1 .text1{ color:black; font:800 20px 'cairo';  }
+                 .posicon {position:relative;}   .iconplus  {color:black; position: absolute; top:0.7em; right:-1em;   }    .iconplushover{display:none;}                  .iconminus {color:black; position: absolute; top:0.7em; right:-1em; display:none;} .iconminushover{display:block;}
+
+
+
+ 				 .bank{   display:none;  justify-content:flex-start; text-align:left;  }    .bankhover{ display:grid; } 
+					 .container4 .row1 .col1 .content1  .bank .btn1{}
+					 .container4 .row1 .col1 .content1  .bank .btn2{}
+									
+
+   
+   
+@media only screen and (max-width:980px)  
+{
+
+}
+
+@media only screen and (max-width:360px)  
+{   
+				
+}
+</style>
+
+
+
+
+<div class="container4">
+  <div class="row1">
+		
+			<div class="col1"> 
+		    <div class="content1">
+				<div class="postext1">
+				  <div class="text1">  Demander un paiement </div>
+				  <a class="posicon">  <i class="iconplus fas fa-plus"></i>  <i class="iconminus fas fa-minus"></i>  </a>
+				  
+				  
+				</div> 
+				<div class="bank">
+				  <div class="btn1">Cash+</div>
+				  <div class="btn2">Virement bancaire</div>
+				</div>
+         	</div> 
+			</div>
+			
+		
+
+		
+   </div>		
+</div>
+<br/><br/><br/>
+
+<script>
+$(".posicon").click (function(){    $(".iconplus").toggleClass("iconplushover");  $(".iconminus").toggleClass("iconminushover");           $(".bank").toggleClass("bankhover");     });
+</script>
+
+
