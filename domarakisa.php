dfc3081ae3529ad184b78efc0c41bb1e5dff870a
MehdiOuatmane <39778027+mehdiouatmane@users.noreply.github.com>
2021-10-02 23:07:20 +0100

    Add files via upload

--- domarakisa.php
+++ domarakisa.php
@@ -78,24 +78,23 @@ if(        isset($_POST["sendinfo"])              )
 <style>html, body{ background-color:transparent;  padding:0; border:0; margin:0;  touch-action: manipulation;    user-select: none;   }   ul li {list-style-type:none;}     </style>
 
 
-<!-- Facebook Pixel Code -->	
+
+
+<!-- Facebook Pixel Code -->
 <script>
-  !function(f,b,e,v,n,t,s)
-  {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-  n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-  if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-  n.queue=[];t=b.createElement(e);t.async=!0;
-  t.src=v;s=b.getElementsByTagName(e)[0];
-  s.parentNode.insertBefore(t,s)}(window, document,'script',
-  'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '1056547344843414');
-  fbq('track', 'PageView');
+!function(f,b,e,v,n,t,s)
+{if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+n.queue=[];t=b.createElement(e);t.async=!0;
+t.src=v;s=b.getElementsByTagName(e)[0];
+s.parentNode.insertBefore(t,s)}(window, document,'script',
+'https://connect.facebook.net/en_US/fbevents.js');
+fbq('init', '974238796457681');
+fbq('track', 'PageView');
 </script>
-<noscript>  <img height="1" width="1" style="display:none"   src="https://www.facebook.com/tr?id=1056547344843414&ev=PageView&noscript=1"/>   </noscript>
-<!-- End Facebook Pixel Code -->	
-
-
-
+<noscript><img height="1" width="1" style="display:none"    src="https://www.facebook.com/tr?id=974238796457681&ev=PageView&noscript=1"/></noscript>
+<!-- End Facebook Pixel Code -->
 
 
 
