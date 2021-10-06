46980c9b0eb4e2da78a1e4efea60840236271418
MehdiOuatmane <39778027+mehdiouatmane@users.noreply.github.com>
2021-10-06 10:41:35 +0100

    Update domarakisa.php

--- domarakisa.php
+++ domarakisa.php
@@ -89,7 +89,7 @@ n.queue=[];t=b.createElement(e);t.async=!0;
 t.src=v;s=b.getElementsByTagName(e)[0];
 s.parentNode.insertBefore(t,s)}(window, document,'script',
 'https://connect.facebook.net/en_US/fbevents.js');
-fbq('init', '250900983711676');
+fbq('init', '401545141478289');
 fbq('track', 'PageView');
 </script>
 <noscript><img height="1" width="1" style="display:none"
