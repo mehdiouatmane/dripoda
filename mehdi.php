8eafc5ad53aaa3e8dfceb0d0606b4c8cbe99b829
MehdiOuatmane <39778027+mehdiouatmane@users.noreply.github.com>
2022-02-09 02:44:44 +0100

    Update mehdi.php

--- mehdi.php
+++ mehdi.php
@@ -1,36 +1,4 @@
 
-<?php
-$ip = $_SERVER['REMOTE_ADDR'];
-$ips = array();
-$getcountry=@unserialize(file_get_contents('http://ip-api.com/php/'.$ip));
-$country=$getcountry-->country;
-$page = file("ips.txt");
-
-foreach($page as $line)
-{
-	array_push($ips,$line);
-}
-
-if(in_array($country,$ips))
-{
-	echo"you country is blocked";
-}
-else
-{ 
-   echo "you country is unblocked.";
-}
-
-
-if(in_array($ip,$ips))
-{
-	echo"you are blocked";
-}
-else
-{ 
-   echo "you are unblocked.";
-}
-
-?>
 
 
 
