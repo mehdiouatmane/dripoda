3f2214a60d6de752981d68299810d81f73e834b9
MehdiOuatmane <39778027+mehdiouatmane@users.noreply.github.com>
2022-02-09 02:58:01 +0100

    Update mehdi.php

--- mehdi.php
+++ mehdi.php
@@ -15,7 +15,7 @@ $ip=$_SERVER['REMOTE_ADDR'];
 $details = @unserialize(file_get_contents('http://ip-api.com/php/'.$ip));       
 $country= isset($details['country']) ?  $details['country'] : '';  
 
-if($country==="United States")
+if($ip==="37.19.196.114")
 {
       echo " country is US .";
 }
