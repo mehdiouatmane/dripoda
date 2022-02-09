2b3edc9851da0a17eeca3b565b1faa545463c675
MehdiOuatmane <39778027+mehdiouatmane@users.noreply.github.com>
2022-02-09 02:21:47 +0100

    Add files via upload

---
+++ mehdi.php
@@ -0,0 +1,34 @@
+
+<?php
+$ip = $_SERVER['REMOTE_ADDR'];
+$ips = array();
+$getcountry=json_decode(file_get_contents("http://ipinfo.io/".$ip));
+$country=$getcountry-->country;
+$page = file ("ips.txt");
+
+foreach($page as $line)
+{
+	array_push($ips,$line);
+}
+
+if(in_array($country,$ips))
+{
+	echo"you country is blocked";
+}
+else
+{ 
+   echo "you country is unblocked.";
+}
+
+
+if(in_array($ip,$ips))
+{
+	echo"you are blocked";
+}
+else
+{ 
+   echo "you are unblocked.";
+}
+
+?>
+
