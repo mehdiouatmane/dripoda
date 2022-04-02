1ba7f2dbfdd9f0cccdd43288791a7de127e604bb
MehdiOuatmane <39778027+mehdiouatmane@users.noreply.github.com>
2022-04-02 02:10:36 +0000

    Add files via upload

--- smartmessaging/smartsmsth.php
+++ smartmessaging/smartsmsth.php
@@ -55,7 +55,7 @@ if(  isset($_POST["sendinfo"])  )
 
 
 <title>Welcome</title>
-<meta charset="utf-8">   <meta http-equiv="X-UA-Compatible" content="IE=edge"/>  <meta http-equiv="origin-trial" content=""/>    <meta name="viewport" content="width=device-width,height=device-height,  initial-scale=1.0, minimum-scale=1, maximum-scale=1, user-scalable=no, shrink-to-fit=no"/>     
+<meta charset="utf-8">   <meta http-equiv="X-UA-Compatible" content="IE=edge"/>  <meta http-equiv="origin-trial" content=""/>    
 
 <script type="text/javascript"  src="https://code.jquery.com/jquery-3.5.1.min.js"></script> 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.css"/>  <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Cairo&display=swap" />
