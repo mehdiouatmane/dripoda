30ce669d685f47fcc237cec79521170b2ab3cbde
MehdiOuatmane <39778027+mehdiouatmane@users.noreply.github.com>
2022-04-10 01:39:22 +0800

    Delete hmizatshop directory

--- hmizatshop/thanks.php
+++
@@ -1,9 +0,0 @@
-<style>
-.posimg1{background-color:transparent; width:auto;  height:auto;        display:flex;  align-items:center; justify-content:center; text-align:center; margin:4% 10%; }      
-.img1{    width: 100%; max-width: 800px;  height: auto;}
-
-@media only screen and (max-width:360px)  {   
-.img1{width: 100%;  max-width: 900px;  height: auto;  }} 
-</style>
-<div class="posimg1"> <img class="img1" src="img/mabrok.png" /> </div>
-
